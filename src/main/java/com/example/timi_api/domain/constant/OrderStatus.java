abc205027630af5e4ca3584a4767640b2ccdb7ac
Phan Hải Đăng <156905818+dangph12@users.noreply.github.com>
2026-07-07 09:15:02 +0700

    feat: add constant

---
+++ src/main/java/com/example/timi_api/domain/constant/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.example.timi_api.domain.constant;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/example/timi_api/domain/constant/Role.java
@@ -0,0 +1,5 @@
+package com.example.timi_api.domain.constant;
+
+public enum Role {
+    ADMIN, USER
+}
